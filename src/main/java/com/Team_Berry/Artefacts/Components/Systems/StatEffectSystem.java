6472a2492dd43b01f8fdf8243a5092143818e136
Nathan Caba <74567210+NeoWaffleSpy@users.noreply.github.com>
2026-03-30 17:51:14 +0200

    Update StatEffectSystem.java

--- src/main/java/com/Team_Berry/Artefacts/Components/Systems/StatEffectSystem.java
+++ src/main/java/com/Team_Berry/Artefacts/Components/Systems/StatEffectSystem.java
@@ -1,6 +1,9 @@
 package com.Team_Berry.Artefacts.Components.Systems;
 
 import com.Team_Berry.Artefacts.ArtefactPlugin;
+import com.Team_Berry.Artefacts.Codecs.ArtefactCodec;
+import com.Team_Berry.Artefacts.Codecs.Enums.TriggerType;
+import com.Team_Berry.Artefacts.Codecs.Stats.StatCodec;
 import com.Team_Berry.Artefacts.Components.Data.StatEffectComponent;
 import com.Team_Berry.SlayTheTower;
 import com.hypixel.hytale.component.*;
@@ -10,6 +13,9 @@ import com.hypixel.hytale.server.core.modules.entity.damage.Damage;
 import com.hypixel.hytale.server.core.modules.entity.damage.DamageEventSystem;
 import com.hypixel.hytale.server.core.modules.entitystats.EntityStatMap;
 import com.hypixel.hytale.server.core.modules.entitystats.EntityStatsModule;
+import com.hypixel.hytale.server.core.modules.entitystats.asset.EntityStatType;
+import com.hypixel.hytale.server.core.modules.entitystats.modifier.Modifier;
+import com.hypixel.hytale.server.core.modules.entitystats.modifier.StaticModifier;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
@@ -49,16 +55,37 @@ public class StatEffectSystem {
                 return;
             else {
                 EntityStatMap statMap = (EntityStatMap)archetypeChunk.getComponent(index, EntityStatsModule.get().getEntityStatMapComponentType());
+                if (statMap == null)
+                    return;
                 comp.artefactUpdated.forEach(artefact -> {
-                    String key = SlayTheTower.get().getName() + "-" + artefact.getId();
-                    artefact.statList.forEach(stat -> {
-                        if (stat == null)
+                    for (int i = 0; i < artefact.statList.size(); i++) {
+                        StatCodec stat = artefact.statList.get(i);
+                        String key = SlayTheTower.get().getName() + "-" + artefact.getId() + "-" + i;
+                        if (stat == null || stat.type == null)
                             return;
-                        //statMap.putModifier(EntityStatType.getAssetMap().getIndex("health"), key)
-                    });
+                        if (stat.trigger == TriggerType.PASSIVE) {
+                            if (!comp.artefactList.containsKey(artefact) || comp.getAmount(artefact) == 0)
+                                statMap.removeModifier(getEntityIndex(stat.type), key);
+                            else
+                                statMap.putModifier(getEntityIndex(stat.type), key, new StaticModifier(Modifier.ModifierTarget.MAX, stat.calc, stat.value * comp.getAmount(artefact)));
+                        }
+                    }
                 });
                 comp.artefactUpdated.clear();
+                /*for (int i = 0; i < statMap.size(); i++) {
+                    String s = statMap.get(i).getId() + " - ";
+                    try {
+                        s += statMap.get(i).getModifiers().toString();
+                    } catch (NullPointerException e) {
+                        s += "None";
+                    }
+                    ArtefactPlugin.LOGGER.atWarning().log(s);
+                }*/
             }
+        }
+
+        private int getEntityIndex(EntityStatType statType) {
+            return ArtefactPlugin.getEntityStatTypeAssetStore().getIndex(statType.getId());
         }
 
         @Override

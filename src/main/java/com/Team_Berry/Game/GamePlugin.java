3f675a9d35b32caa4760c7f1b00e11c01c9c6a98
Nathan Caba <74567210+NeoWaffleSpy@users.noreply.github.com>
2026-04-28 01:56:03 +0200

    Rain Interaction

--- src/main/java/com/Team_Berry/Game/GamePlugin.java
+++ src/main/java/com/Team_Berry/Game/GamePlugin.java
@@ -91,6 +91,7 @@ public class GamePlugin extends JavaPlugin {
         this.getCodecRegistry(Interaction.CODEC).register("ClaimStartWeaponInteraction", ClaimStartWeaponInteraction.class, ClaimStartWeaponInteraction.CODEC);
         this.getCodecRegistry(Interaction.CODEC).register("KweebecMerchantInteraction", KweebecMerchantInteraction.class, KweebecMerchantInteraction.CODEC);
         this.getCodecRegistry(Interaction.CODEC).register("SpawnAOEInteraction", SpawnAOEInteraction.class, SpawnAOEInteraction.CODEC);
+        this.getCodecRegistry(Interaction.CODEC).register("ProjectileRainInteraction", ProjectileRainInteraction.class, ProjectileRainInteraction.CODEC);
 
         this.getCommandRegistry().registerCommand(new GetParticipantsIndices("getparticipantsindices", "get all indexes of all active participants"));
 
---
+++ src/main/java/com/Team_Berry/Game/Interactions/ProjectileRainInteraction.java
@@ -0,0 +1,127 @@
+package com.Team_Berry.Game.Interactions;
+
+import com.Team_Berry.Game.GamePlugin;
+import com.hypixel.hytale.codec.Codec;
+import com.hypixel.hytale.codec.KeyedCodec;
+import com.hypixel.hytale.codec.builder.BuilderCodec;
+import com.hypixel.hytale.codec.validation.Validators;
+import com.hypixel.hytale.component.AddReason;
+import com.hypixel.hytale.component.CommandBuffer;
+import com.hypixel.hytale.component.Holder;
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.math.vector.Transform;
+import com.hypixel.hytale.math.vector.Vector3d;
+import com.hypixel.hytale.math.vector.Vector3f;
+import com.hypixel.hytale.protocol.InteractionType;
+import com.hypixel.hytale.server.core.asset.type.projectile.config.Projectile;
+import com.hypixel.hytale.server.core.entity.Entity;
+import com.hypixel.hytale.server.core.entity.EntityUtils;
+import com.hypixel.hytale.server.core.entity.InteractionContext;
+import com.hypixel.hytale.server.core.entity.LivingEntity;
+import com.hypixel.hytale.server.core.entity.UUIDComponent;
+import com.hypixel.hytale.server.core.entity.entities.ProjectileComponent;
+import com.hypixel.hytale.server.core.modules.entity.component.Intangible;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
+import com.hypixel.hytale.server.core.modules.projectile.config.BallisticData;
+import com.hypixel.hytale.server.core.modules.projectile.config.BallisticDataProvider;
+import com.hypixel.hytale.server.core.modules.time.TimeResource;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import com.hypixel.hytale.server.core.util.TargetUtil;
+import java.util.UUID;
+import java.util.random.RandomGenerator;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class ProjectileRainInteraction extends SimpleInstantInteraction implements BallisticDataProvider {
+    @Nonnull
+    public static final BuilderCodec<ProjectileRainInteraction> CODEC;
+    protected String projectileId;
+    protected int projectileCount = 0;
+    protected float angleOffset = 0.1f;
+
+    private final float MAX_PITCH_ANGLE = 1.5607964f;
+
+    public String getProjectileId() {
+        return this.projectileId;
+    }
+
+    @Nullable
+    public BallisticData getBallisticData() {
+        return Projectile.getAssetMap().getAsset(this.projectileId);
+    }
+
+    protected void firstRun(@Nonnull InteractionType type, @Nonnull InteractionContext context, @Nonnull CooldownHandler cooldownHandler) {
+        CommandBuffer<EntityStore> commandBuffer = context.getCommandBuffer();
+
+        assert commandBuffer != null;
+
+        World world = commandBuffer.getExternalData().getWorld();
+        Ref<EntityStore> sourceRef = context.getEntity();
+        Entity var8 = EntityUtils.getEntity(sourceRef, commandBuffer);
+        if (var8 instanceof LivingEntity) {
+            Transform lookVec = TargetUtil.getLook(sourceRef, commandBuffer);
+            Vector3d lookPosition = lookVec.getPosition();
+            UUIDComponent sourceUuidComponent = commandBuffer.getComponent(sourceRef, UUIDComponent.getComponentType());
+            if (sourceUuidComponent != null) {
+                UUID sourceUuid = sourceUuidComponent.getUuid();
+                TimeResource timeResource = commandBuffer.getResource(TimeResource.getResourceType());
+
+                for (Vector3f lookRotation : circleDirections()) {
+                    Holder<EntityStore> holder = ProjectileComponent.assembleDefaultProjectile(timeResource, this.projectileId, lookPosition, lookRotation);
+                    ProjectileComponent projectileComponent = holder.getComponent(ProjectileComponent.getComponentType());
+
+                    assert projectileComponent != null;
+
+                    holder.ensureComponent(Intangible.getComponentType());
+                    if (projectileComponent.getProjectile() == null) {
+                        projectileComponent.initialize();
+                        if (projectileComponent.getProjectile() == null) {
+                            return;
+                        }
+                    }
+                    GamePlugin.LOGGER.atSevere().log(lookRotation.getYaw() + " - " + lookRotation.getPitch());
+                    projectileComponent.shoot(holder, sourceUuid, lookPosition.getX(), lookPosition.getY(), lookPosition.getZ(), lookRotation.getYaw(), lookRotation.getPitch());
+                    commandBuffer.addEntity(holder, AddReason.SPAWN);
+                }
+            }
+        }
+    }
+
+    public Vector3f[] circleDirections() {
+        var rng = RandomGenerator.getDefault();
+        var directions = new Vector3f[projectileCount];
+
+        float pitch = MAX_PITCH_ANGLE - rng.nextFloat() * angleOffset;
+        for (int i = 0; i < projectileCount; i++) {
+            float yaw   = (float) (Math.PI - (2 * Math.PI * i / projectileCount));
+
+            directions[i] = new Vector3f();
+            directions[i].setPitch(pitch);
+            directions[i].setYaw(yaw);
+        }
+
+        return directions;
+    }
+
+    protected void simulateFirstRun(@Nonnull InteractionType type, @Nonnull InteractionContext context, @Nonnull CooldownHandler cooldownHandler) {
+    }
+
+    static {
+        CODEC = BuilderCodec.builder(ProjectileRainInteraction.class, ProjectileRainInteraction::new, SimpleInstantInteraction.CODEC)
+                .documentation("Launches a projectile.")
+                .append(new KeyedCodec<>("ProjectileId", Codec.STRING),
+                        (i, o) -> i.projectileId = o,
+                        (i) -> i.projectileId)
+                .addValidator(Validators.nonNull())
+                .addValidator(Projectile.VALIDATOR_CACHE.getValidator().late()).add()
+                .append(new KeyedCodec<>("Projectile Count", Codec.INTEGER),
+                        (i, o) -> i.projectileCount = o,
+                        (i) -> i.projectileCount).add()
+                .append(new KeyedCodec<>("Angle Offset", Codec.FLOAT),
+                        (i, o) -> i.angleOffset = o,
+                        (i) -> i.angleOffset).add()
+                .build();
+    }
+}

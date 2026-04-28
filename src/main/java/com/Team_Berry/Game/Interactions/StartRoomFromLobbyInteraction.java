e1d818e2e1c43ab42a372abaad9bd7e10c4c42b0
MadJack01 <giozdaou@gmail.com>
2026-04-28 22:01:42 +0200

    portals activated by squirrels fixed lol

--- src/main/java/com/Team_Berry/Game/Interactions/StartRoomFromLobbyInteraction.java
+++ src/main/java/com/Team_Berry/Game/Interactions/StartRoomFromLobbyInteraction.java
@@ -8,6 +8,7 @@ import com.hypixel.hytale.protocol.InteractionType;
 import com.hypixel.hytale.server.core.entity.InteractionContext;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.jetbrains.annotations.NotNull;
@@ -20,7 +21,8 @@ public class StartRoomFromLobbyInteraction extends SimpleInstantInteraction {
     protected void firstRun(@NotNull InteractionType interactionType, @NotNull InteractionContext interactionContext, @NotNull CooldownHandler cooldownHandler) {
         CommandBuffer<EntityStore> commandBuffer = interactionContext.getCommandBuffer();
         World world = commandBuffer.getStore().getExternalData().getWorld();
-
+        PlayerRef playerRef = commandBuffer.getComponent(interactionContext.getOwningEntity(), PlayerRef.getComponentType());
+        if (playerRef == null) return;
         if (world != null) {
             GameManager manager = GamePlugin.get().getGameManagers().get(world);
             if (manager != null) {
